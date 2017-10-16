c6699550a794332aa24995dedc6ecc6d67de3f71
CSE\dxz149 <dxz149@psu.edu>
2017-10-16 10:47:58 -0400

    assign randim capacity to a graph

---
+++ Backup/Backup/Backup/assign_random_capacity.py
@@ -0,0 +1,25 @@
+import pydot # import pydot or you're not going to get anywhere my friend :D
+import networkx as nx
+import my_lib as my_lib_var
+import sys
+#import winsound
+import time
+from scipy import stats
+from my_lib import *
+from my_flows_lib import *
+from my_lib_optimal_recovery import *
+from my_lib_optimal_recovery_multicommodity import *
+from my_lib_optimal_recovery_multicommodity_worst import *
+from my_lib_optimal_recovery_multicommodity_best import *
+from my_lib_check_routability import *
+from my_lib_compute_max_demand_in_the_graph import *
+
+#filename_graph= '600_800_Random_Capacity'
+filename_graph='erdos_renyi_graph_100_nodes_231_edges_Random' #'Abilene'
+
+path_to_graph= 'network topologies/'
+path_to_graph=path_to_graph+filename_graph+'.gml'
+H=nx.MultiGraph(nx.read_gml(path_to_graph))
+
+
+assign_random_capacity_to_edges(H,filename_graph)
