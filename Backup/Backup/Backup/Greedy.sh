57981e3cac4c15dbeb0fd049176b44971e9a537a
CSE\dxz149 <dxz149@psu.edu>
2017-10-16 10:48:28 -0400

    Greedy bash script

---
+++ Backup/Backup/Backup/Greedy.sh
@@ -0,0 +1,4 @@
+Greedy:
+ \python multiple_simulations_SDN_erdos_231.py -1 erdos_renyi_graph_100_nodes_231_edges 5 unknown_capacity diman 0 0 20 1.0 0 0.001 0
+OPT:
+ \python multiple_simulations_SDN_erdos_231_OPT.py -1 erdos_renyi_graph_100_nodes_231_edges 7 unknown_capacity diman 0 0 20 1.0 0 0.001 0
