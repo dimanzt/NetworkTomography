e75b45083ec0c79474cfaef07ab5b11733414fd0
CSE\dxz149 <dxz149@psu.edu>
2018-04-20 17:00:29 -0400

    async sh

---
+++ Backup/Backup/Backup/SDN_async.sh
@@ -0,0 +1,4 @@
+\python multiple_SDN_async.py -1 BellCanada_Random_Capacity_High_Capacity_2_backbone_eterogeneous 4 unknown_capacity diman 0 0 22 0.1 0.5 0
+
+# the last value=error is the amount of error with respect to optimal path length!
+
